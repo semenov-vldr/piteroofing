bb181cd3e11061461c0715b2e7c8802c3c793c67
Vladimir <vova2066@yandex.ru>
2023-02-03 23:34:03 +0300

    add mobile menu

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+const header = document.querySelector('header.header');
+
+if (header) {
+
+  const headerBurger = header.querySelector('.header__burger');
+
+  headerBurger.addEventListener('click', () => {
+    header.classList.toggle('js-menu-open');
+    toggleScrollBody();
+  });
+
+
+  // scroll
+  window.addEventListener('scroll', () => scrollHeader (header) );
+
+}
